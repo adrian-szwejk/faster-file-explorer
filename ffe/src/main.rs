f35e246e1c968ba858e94aeb226be056f22334d9
adrian-szwejk <aszwe2@illinois.edu>
2023-07-23 13:41:11 -0500

    fixed issue with printing all directories from depth >=1 to only do 1st depth

--- ffe/src/main.rs
+++ ffe/src/main.rs
@@ -1,11 +1,11 @@
 //use std::env;
-use std::fs;
-use std::io;
+//use std::fs;
+//use std::io;
 
 extern crate glob;
-use glob::glob;
+//use glob::glob;
 extern crate walkdir;
-use walkdir::WalkDir;
+use walkdir::{DirEntry, WalkDir};
 use std::path::{Path, PathBuf};
 
 extern crate directories;
@@ -40,30 +40,56 @@ fn main() {
     // }
 
     
-    println!("BaseDirs:");
+    println!("UserDirs:");
     let music_path = get_music_path();
- 
-    for i in WalkDir::new(music_path).into_iter().filter_map(|e| e.ok()) {
-        if i.metadata().unwrap().is_file() {
-            println!("{}", i.path().display());
-        }
-    }
+    
+    WalkDir::new(music_path)
+    .min_depth(0)
+    .max_depth(1)
+    .into_iter()
+    //.filter_entry(|e| is_not_hidden(e))
+    .filter_map(|v| v.ok())
+    .for_each(|x| println!("{}", x.path().display()));
+    
+
+    let c:PathBuf = get_base_path();
+    println!("BaseDirs:");
+    // for i in WalkDir::new(c).into_iter().filter_map(|e| e.ok()) {
+    //     if i.metadata().unwrap().is_file() {
+    //         println!("{}", i.path().display());
+    //     }
+    // }
                                                                   
     //Used to find global directories
 }
 
 //cargo run -- test test_doc.txt
 
+
 fn get_music_path() -> PathBuf{
     let mut music_path= PathBuf::new(); 
+            
     if let Some(user_dirs) = UserDirs::new() {
-        if let Some(i) = user_dirs.audio_dir() {
-            //println!("{:?}",i);
-            music_path=i.to_path_buf();
-        }    
-        // Lin: /home/alice/Music
-        // Win: C:\Users\Alice\Music
-        // Mac: /Users/Alice/Music
+        //println!("{}", user_dirs.home_dir().display());
+        music_path = user_dirs.home_dir().to_path_buf();
     }
     music_path
+}
+
+fn get_base_path() -> PathBuf{
+    let mut c_drive= PathBuf::new(); 
+    if let Some(base_dirs) = BaseDirs::new() {
+        c_drive=base_dirs.home_dir().to_path_buf();                                             
+    }
+    c_drive
+}
+
+//Function to get rid of hidden files directories
+//TODO: This also gets rid of ./ folder directories
+fn is_not_hidden(entry: &DirEntry) -> bool {
+    entry
+         .file_name()
+         .to_str()
+         .map(|s| entry.depth() == 0 || !s.starts_with("."))
+         .unwrap_or(false)
 }
